750857a27ee3413c89325cd92725f7d4f55877d9
Gautam0104 <111377889+Gautam0104@users.noreply.github.com>
2023-06-09 23:04:44 +0530

    assemble pages

---
+++ commonpage/header.php
@@ -0,0 +1,31 @@
+<!-- header -->
+
+<div class="container-fluid d-flex align-items-center justify-content-between">
+
+<a class="logo d-flex align-items-center me-auto me-xl-0" href="index.php">
+
+<h1>Substring<br>Technology</h1>
+  <span>.</span>
+</a>
+
+<!-- Nav Menu -->
+<nav id="navmenu" class="navmenu">
+  <ul>
+    <li><a href="index.php#hero" class="active">Home</a></li>
+    <li><a href="index.php#services">Services</a></li>
+    <li><a href="index.html#services">Develoment</a></li>
+    <li><a href="index.html#portfolio">Training</a></li>
+    <li><a href="index.html#team">Intership</a></li>
+    <li><a href="blog.html">Placement</a></li>
+    <li><a href="blog.html">Gallery</a></li>
+    <li><a href="blog.html">Career</a></li>
+    <li><a href="index.php#about">AboutUs</a></li>
+    <li><a href="forms/contact.php">ContactUs</a></li>
+  </ul>
+
+  <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+</nav><!-- End Nav Menu -->
+
+<a href="index.html#about" class="btn-getstarted">Get Started</a>
+
+</div>
