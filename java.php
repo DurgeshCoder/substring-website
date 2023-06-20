803007ee5e10371dd608d448b3553c3a453b9b95
Gautam shukla <gautamshukla263@gmail.com>
2023-06-20 17:36:39 +0530

    training page added

---
+++ java.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "components/important_links.php" ?>
+
+    <title>java Training</title>
+</head>
+<body>
+  <!-- ======= Header ======= -->
+  <header class="header d-flex align-items-center sticky-top" id="header">
+   <?php include "components/header.php" ?>
+  </header>
+  <!-- End Header -->
+
+    <!-- Training Banner Section - Offline training Page -->
+    <section id="java_training_banner" class="java_training_banner">
+
+  
+
+      <div class="container ">
+        <div class="row justify-space-between" >
+          
+          <div class="col-lg-7">
+            <h2 data-aos="fade-up" data-aos-delay="100">Digital Ocean Series: Deployment on Cloud</h2>
+            <p data-aos="fade-up" data-aos-delay="200">This course is focused on providing 
+              hands-on experience with deploying applications on DigitalOcean's
+               cloud platform using Droplets. Students will learn how to create 
+               and configure Droplets, connect to them via SSH, and use popular 
+               tools such as Git, Nginx, and Node.js to deploy web applications. 
+               The course will also cover best practices for securing and managing
+                Droplets, as well as scaling and monitoring applications. By the end
+                 of the course,
+               students will have the skills and knowledge necessary to deploy
+                and manage their own applications on....</p>
+               <div class="btn">
+               <a href="" class="f-4 decoration"> Rating</a>
+               </div>
+          </div>
+         
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </section><!-- End Training Banner Section -->
+
+   <div class="java_training_card d-flex ">
+  <div class="row justify-content-end align-item-end">
+  <div class="col-lg-4 my-2">
+          <div class="card">
+            <div class="img">
+              <img src="images/t_subject.png.crdownload" class="w-100 rounded-4">
+            </div>
+            <div class="button m-auto my-4">
+              <button class="btn btn-primary">Start Learning</button>
+            </div>
+            <div class="section-tittle">
+              <h2>Features of this Course :</h2>
+              <p>18 + Quality Videos</p>
+              <p>1000 + Enrollments</p>
+              <p>5 out of 5 Students Reviews</p>
+              <p>Course will be in Hindi</p>
+</div>
+  </div>
+   </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <?php include "components/footer.php" ?>
+
+  </footer><!-- End Footer -->
+
+<!-- Scroll Top Button -->
+<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Preloader -->
+<div id="preloader">
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+</div>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
+
+<script>
+        
+
+            </script>
+
+</body>
+</html>
+
+
