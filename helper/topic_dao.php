aa73127a5ca72469a32bb6a7549e284997dfcd25
Gautam shukla <gautamshukla263@gmail.com>
2023-07-06 12:15:04 +0530

    add topic on working

--- helper/topic_dao.php
+++ helper/topic_dao.php
@@ -22,19 +22,19 @@ class TopicDao{
 
 
     }
+    function _topic_subtopic(){
 
+        $query="select subjects.sub_name,subjects.short_description,subjects.long_description,subjects.language,subjects.cover_img,subjects.rating,topic.top_id,topic.topic_tittle,sub_toppic.sub_top_id,sub_toppic.sub_topic_tittle from subjects INNER JOIN topic  ON
+        topic.sub_id=subjects.sub_id INNER JOIN sub_toppic ON topic.top_id=sub_toppic.top_id ";
+        $statement=$this->conn->prepare($query);
+    
+        $statement->execute();
+        $result=$statement->fetchAll();
+        return $result;
+        }
 
 }
 
-function _topic_subtopic($slug){
 
-    $query="select subjects.sub_name,subjects.short_description,subjects.long_description,subjects.language,subjects.cover_img,subjects.rating,topic.top_id,topic.topic_tittle,sub_toppic.sub_top_id,sub_toppic.sub_topic_tittle from subjects INNER JOIN topic  ON
-    topic.sub_id=subjects.sub_id INNER JOIN sub_toppic ON topic.top_id=sub_toppic.top_id where subjects.slug=?";
-    $statement=$this->conn->prepare($query);
-    $statement->bindValue(1, $slug);
-    $statement->execute();
-    $result=$statement->fetchAll();
-    return $result;
-    }
 
 ?>
