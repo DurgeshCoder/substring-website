f0c78b34207211a47977083fe4b5efea02e3625c
Gautam shukla <gautamshukla263@gmail.com>
2023-07-06 09:29:41 +0530

    add subject page completed

---
+++ admin/add_topic.php
@@ -0,0 +1,33 @@
+<?php
+
+include "../helper/config.php";
+
+?>
+
+<!-- select box  -->
+
+<div>
+    <label for="">Subject</label>
+    <select name="" id="subject_select_box" class="form-control >
+     <option value=""></option>
+</select>
+
+</div>
+
+
+<script src=" <?= $base_url ?>/admin/scripts/admin.js"></script>
+        <script>
+            get_subjects().then(data => {
+
+                console.log(data)
+                const selectBox = document.getElementById("subject_select_box")
+                let elements = `<option disabled selected>---select subject---</option>`;
+                data.forEach(item => {
+                    elements += `<option>${item.sub_  name
+                } </option > `;
+            });
+            selectBox.innerHTML=elements;
+
+
+        });
+        </script>
